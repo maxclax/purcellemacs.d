bfec7d11475de5e76ce2ec6d25798eb656fbc3ea
dev <dev>
2025-07-01 22:21:23 +0200

    fix: fonts and elein

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,3 +1,22 @@
+;;; init-local.el --- own -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
+;; Set default font
+(set-face-attribute 'default nil
+					:family "Berkeley Mono Variable"
+					:height 180
+					:weight 'normal
+					:width 'normal)
+
+;; Set font for new frames
+(add-to-list 'default-frame-alist 
+			 '(font . "Berkeley Mono Variable-18"))
+
+;; Optional: Set font for specific faces
+(set-face-attribute 'fixed-pitch nil
+					:family "Berkeley Mono Variable"
+					:height 180))
 
 (provide 'init-local)
+;;; init-local.el ends here
